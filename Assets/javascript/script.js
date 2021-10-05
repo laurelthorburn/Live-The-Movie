21580171c69c56ffe192fa07989fb1448b3d579b
Laurel Thorburn <laurelthorburn@gmail.com>
2021-10-04 19:56:08 -0600

    Centered cards

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,3 +1,3 @@
-var APIFood = 3d950f2860e74e5a949e59ac3b5f4126;
-var APIMovie = ;
-var APIMusic = ;
+var APIFood = "3d950f2860e74e5a949e59ac3b5f4126"; //Matt
+var APIMovie = ; //Valerii
+var APIMusic = "514c0e2f2fa54a6fbbce56cbac5b202e"; // Cameron
