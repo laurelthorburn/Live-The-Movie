241d570d577ee18e11c6db6aac376ae889ec1ba0
Laurel Thorburn <laurelthorburn@gmail.com>
2021-10-04 21:07:13 -0600

    Styling main page

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,3 +1,3 @@
 var APIFood = "3d950f2860e74e5a949e59ac3b5f4126"; //Matt
-var APIMovie = ; //Valerii
+var APIMovie = "d58ec33864c2c1ca7cfddcf6e0b283c8"; //Valerii
 var APIMusic = "514c0e2f2fa54a6fbbce56cbac5b202e"; // Cameron
