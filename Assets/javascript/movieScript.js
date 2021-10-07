d60a13f00102d8a88f217e46cbfb56386dd6bee7
Valllerian <88560096+Valllerian@users.noreply.github.com>
2021-10-06 20:35:34 -0600

    Fetching arrays for every movie genre works in console log (#18)
    * Testing git merge
    
    * Working on fetching simple movie lists (sorted by genre)
    
    * Every buttons now generates an array with movies connected to the holiday
    
    * Getting rid of genres - using search quary
    
    Co-authored-by: Laurel Thorburn <88687282+laurelthorburn@users.noreply.github.com>

--- Assets/javascript/movieScript.js
+++ Assets/javascript/movieScript.js
@@ -10,83 +10,155 @@ const searchURL = baseUrl + '/search/movie?'+ apiKey;
 // Element that will show our search results
 const mainMovie = document.getElementById('mainMovie');
 
-// fetched from https://api.themoviedb.org/3/genre/movie/list?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8
+// fetched genres from https://api.themoviedb.org/3/genre/movie/list?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8
 
-const genres = [
-    {
-      "id": 28,
-      "name": "Action"
-    },
-    {
-      "id": 12,
-      "name": "Adventure"
-    },
-    {
-      "id": 16,
-      "name": "Animation"
-    },
-    {
-      "id": 35,
-      "name": "Comedy"
-    },
-    {
-      "id": 80,
-      "name": "Crime"
-    },
-    {
-      "id": 99,
-      "name": "Documentary"
-    },
-    {
-      "id": 18,
-      "name": "Drama"
-    },
-    {
-      "id": 10751,
-      "name": "Family"
-    },
-    {
-      "id": 14,
-      "name": "Fantasy"
-    },
-    {
-      "id": 36,
-      "name": "History"
-    },
-    {
-      "id": 27,
-      "name": "Horror"
-    },
-    {
-      "id": 10402,
-      "name": "Music"
-    },
-    {
-      "id": 9648,
-      "name": "Mystery"
-    },
-    {
-      "id": 10749,
-      "name": "Romance"
-    },
-    {
-      "id": 878,
-      "name": "Science Fiction"
-    },
-    {
-      "id": 10770,
-      "name": "TV Movie"
-    },
-    {
-      "id": 53,
-      "name": "Thriller"
-    },
-    {
-      "id": 10752,
-      "name": "War"
-    },
-    {
-      "id": 37,
-      "name": "Western"
-    }
-  ]
+
+  const valentinesDay = $('#Valentines');
+  const Patrick = $('#Patrick');
+  const Independence = $('#Independence');
+  const Halloween = $('#Halloween');
+  const Thanksgiving = $('#Thanksgiving');0
+  const Christmas = $('#Christmas');
+  const NewYear = $('#NewYear');
+  const Muertos = $('#Muertos');
+
+  // valentinesDay.addEventListener("click", openPage());
+
+  // function openPage() {
+    
+  // }
+
+  $('#Valentines').on('click', function(){
+    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
+       // set up API fetch depending on holiday parameters
+       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Valentines&page=1&include_adult=false"
+      //  change ID automatically would be great
+
+       fetch(finalUrl)
+       .then(function (response) {
+         // console.log(response)
+       return response.json();
+       })
+       .then(function (data) {
+         console.log(data)
+         }
+   )
+   });
+
+  $('#Patrick').on('click', function(){
+    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
+       // set up API fetch depending on holiday parameters
+       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Patrick&page=1&include_adult=false"
+       //  change ID automatically would be great
+ 
+        fetch(finalUrl)
+        .then(function (response) {
+          // console.log(response)
+        return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+          }
+    )
+    });
+
+  $('#Halloween').on('click', function(){
+    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
+       // set up API fetch depending on holiday parameters
+       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Halloween&page=1&include_adult=false"
+       //  change ID automatically would be great
+ 
+        fetch(finalUrl)
+        .then(function (response) {
+          // console.log(response)
+        return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+          }
+    )
+    });
+
+  $('#Thanksgiving').on('click', function(){
+    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
+       // set up API fetch depending on holiday parameters
+       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Thanksgiving&page=1&include_adult=false"
+       //  change ID automatically would be great
+ 
+        fetch(finalUrl)
+        .then(function (response) {
+          // console.log(response)
+        return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+          }
+    )
+    });
+
+  $('#Christmas').on('click', function(){
+    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
+       // set up API fetch depending on holiday parameters
+       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Christmas&page=1&include_adult=false"
+       //  change ID automatically would be great
+ 
+        fetch(finalUrl)
+        .then(function (response) {
+          // console.log(response)
+        return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+          }
+    )
+    });
+
+  $('#NewYear').on('click', function(){
+    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
+       // set up API fetch depending on holiday parameters
+       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=NewYearEve&page=1&include_adult=false"
+       //  change ID automatically would be great
+ 
+        fetch(finalUrl)
+        .then(function (response) {
+          // console.log(response)
+        return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+          }
+    )
+    });
+
+  $('#Independence').on('click', function(){
+    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
+       // set up API fetch depending on holiday parameters
+       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=IndependenceDay&page=1&include_adult=false"
+       //  change ID automatically would be great
+ 
+        fetch(finalUrl)
+        .then(function (response) {
+          // console.log(response)
+        return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+          }
+    )
+    });
+  $('#Muertos').on('click', function(){
+    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
+       // set up API fetch depending on holiday parameters
+       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=birthday&page=1&include_adult=false"
+       //  change ID automatically would be great
+ 
+        fetch(finalUrl)
+        .then(function (response) {
+          // console.log(response)
+        return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+          }
+    )
+    });
