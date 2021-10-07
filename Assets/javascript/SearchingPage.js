c19f4945834d15cc67800df124e3ee9fcba5592e
Valerii Bihun <valeriibihun.co@gmail.com>
2021-10-06 20:02:41 -0600

    Getting rid of genres - using search quary

--- Assets/javascript/SearchingPage.js
+++ Assets/javascript/SearchingPage.js
@@ -12,84 +12,6 @@ const mainMovie = document.getElementById('mainMovie');
 
 // fetched from https://api.themoviedb.org/3/genre/movie/list?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8
 
-const genres = [
-    {
-      "id": 28,
-      "name": "Action"
-    },
-    {
-      "id": 12,
-      "name": "Adventure"
-    },
-    {
-      "id": 16,
-      "name": "Animation"
-    },
-    {
-      "id": 35,
-      "name": "Comedy"
-    },
-    {
-      "id": 80,
-      "name": "Crime"
-    },
-    {
-      "id": 99,
-      "name": "Documentary"
-    },
-    {
-      "id": 18,
-      "name": "Drama"
-    },
-    {
-      "id": 10751,
-      "name": "Family"
-    },
-    {
-      "id": 14,
-      "name": "Fantasy"
-    },
-    {
-      "id": 36,
-      "name": "History"
-    },
-    {
-      "id": 27,
-      "name": "Horror"
-    },
-    {
-      "id": 10402,
-      "name": "Music"
-    },
-    {
-      "id": 9648,
-      "name": "Mystery"
-    },
-    {
-      "id": 10749,
-      "name": "Romance"
-    },
-    {
-      "id": 878,
-      "name": "Science Fiction"
-    },
-    {
-      "id": 10770,
-      "name": "TV Movie"
-    },
-    {
-      "id": 53,
-      "name": "Thriller"
-    },
-    {
-      "id": 10752,
-      "name": "War"
-    },
-    {
-      "id": 37,
-      "name": "Western"
-    }
-  ]
 
 
   const valentinesDay = $('#Valentines');
@@ -102,5 +24,5 @@ const genres = [
   const Muertos = $('#Muertos');
 
 function onLoad (){
-
+  // displaying API fetched before using parameters from onClick event from last page
 }
