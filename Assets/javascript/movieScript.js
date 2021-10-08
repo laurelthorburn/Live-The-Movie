6d11706601f4edb7e99e0dd7dd28f1244f4717c4
Valerii Bihun <valeriibihun.co@gmail.com>
2021-10-08 15:11:31 -0600

    Added trailer display

--- Assets/javascript/movieScript.js
+++ Assets/javascript/movieScript.js
@@ -162,6 +162,70 @@ const mainMovie = document.getElementById('mainMovie');
     )
     });
 
+    $('#test').on('click', function(){
+      // set up API fetch depending on holiday parameters
+      var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=2020&page=1&include_adult=false"
+      ;
+      //  change ID automatically would be great
+       fetch(finalUrl)
+         .then(function (response) {
+          // console.log(response)
+       return response.json();
+         })
+         .then(function (data) {
+           console.log(data)
+           var i = 1;
+           var imgLink = "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path;
+           
+           var description = data.results[i].overview;
+           var title = data.results[i].original_title
+           
+           var rating = data.results[i].vote_average
+           //  works for description fetch and display
+           $('#description').text(description);
+          //  works for title fetch and display
+           $('#title').text(title);
+          //  doesnt work
+           $('#posterIMGcard').attr("src", imgLink)
+           $('#rating').text("Rating: " + rating);
+         
+         })
+     });
+
+    
+
+     $('#movie-trailer').on('click', function(){
+      var trailer = "https://api.themoviedb.org/3/movie/716788/videos?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US"
+      fetch(trailer).then(res => res.json()).then(videoData => {
+          if(videoData.results.length > 0){
+            var embed = [];
+            var dots = [];
+            videoData.results.forEach((video, idx) => {
+              let {name, key, site} = video
+    
+              if(site == 'YouTube'){
+                  
+                $('#videoTag').attr("src", "https://www.youtube.com/embed/" + video.key);
+                console.log(video.key)
+                console.log(video.name)
+                
+              //   embed.push(`
+              //     <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              
+              // `)
+    
+              //   dots.push(`
+              //     <span class="dot">${idx + 1}</span>
+              //   `)
+
+              //   console.log(video.site)
+
+              
+              
+              }
+            })
+        }
+     })});
 
 
 
