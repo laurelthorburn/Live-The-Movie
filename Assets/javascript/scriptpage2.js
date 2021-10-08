327eb087071df143929ea7a4eabc9bf9331929a9
Laurel Thorburn <laurelthorburn@gmail.com>
2021-10-07 20:43:35 -0600

    Added links to images

--- Assets/javascript/scriptpage2.js
+++ Assets/javascript/scriptpage2.js
@@ -34,24 +34,30 @@ return response.json();
 })
 .then(function (data) {
  //picture and title one
+    var linkOne = document.createElement('a');
     var pictureOne = document.createElement('img');
     var titleOne = document.createElement('h3');
 
+    linkOne.setAttribute("href","https://spoonacular.com/recipes/" + data.results[0].title + "-" + data.results[0].id);
     pictureOne.setAttribute("src", data.results[0].image);
     titleOne.innerText = data.results[0].title;
 
+    linkOne.append(pictureOne);
     displayFood.append(titleOne);
-    displayFood.append(pictureOne); 
+    displayFood.append(linkOne); 
 
  //picture and title two
+ var linkTwo = document.createElement('a');
  var pictureTwo = document.createElement('img');
  var titleTwo = document.createElement('h3');
 
+ linkTwo.setAttribute("href","https://spoonacular.com/recipes/" + data.results[1].title + "-" + data.results[1].id);
  pictureTwo.setAttribute("src", data.results[1].image);
  titleTwo.innerText = data.results[1].title;
 
+ linkTwo.append(pictureTwo);
  displayFood.append(titleTwo);
- displayFood.append(pictureTwo); 
+ displayFood.append(linkTwo); 
 
  console.log("https://spoonacular.com/recipes/" + data.results[1].title + "-" + data.results[1].id)
 
