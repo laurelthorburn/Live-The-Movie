8cd9223b497ecfa71a1ac2178f975b7e3ec0519d
Valllerian <88560096+Valllerian@users.noreply.github.com>
2021-10-09 14:13:49 -0600

    Fixed the poster issue (#38)
    * Testing git merge
    
    * Working on fetching simple movie lists (sorted by genre)
    
    * Every buttons now generates an array with movies connected to the holiday
    
    * Getting rid of genres - using search quary
    
    * updating vat names
    
    * Displaying dara on second page
    
    * Updating changes
    
    * Added trailer display
    
    * Working on display function for all the objects
    
    * Function for displaying is created
    
    * Display function works on pageload
    
    * Trailer displaying in modal
    
    * fixed NewYears section
    
    * Fixed broken posters issue
    
    Co-authored-by: Laurel Thorburn <88687282+laurelthorburn@users.noreply.github.com>

--- Assets/javascript/scriptpage2.js
+++ Assets/javascript/scriptpage2.js
@@ -17,9 +17,7 @@ const apiUrl = baseUrl + '/discover/movie?sort_by=popularity.desc&'+ apiMovieKey
 const posterUrl = 'https://image.tmdb.org/t/p/w500';
 
   var userChoice = localStorage.getItem('savedHoliday');
-  var query = userChoice
   
-
 //dictionary
 var objectFood = {
     valentines: "heart", 
@@ -34,7 +32,7 @@ var objectFood = {
 
 var objectMovie = {
     valentines: "valentines", 
-    patrick: "patrick",
+    patrick: "st-patrick",
     christmas: "christmas", 
     halloween: "halloween",
     birthday: "birthday", 
@@ -100,7 +98,9 @@ $('#go-back').click(function(){
 function showMovie(){
   
     var finalUrl =   searchUrl + apiMovieKey + language + "&query=" + objectMovie[savedHoliday]  + "&page=1&include_adult=false"
-    console.log ("query is " + query);
+
+    console.log ("query is " + objectMovie[savedHoliday]);
+
     console.log(finalUrl)
     fetch(finalUrl)
     .then(function (response) {
@@ -108,9 +108,11 @@ function showMovie(){
     })
     .then(function (data) {
         console.log(data)
-
+        
       $(".card").each(function (i) {
-  
+        if (data.results[i].poster_path == null){
+          i = i + 2;
+        }
         var imgLink = "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path;
         var description = data.results[i].overview;
         var title = data.results[i].original_title
