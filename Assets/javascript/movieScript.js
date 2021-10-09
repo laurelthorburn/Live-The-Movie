d1a41b7ed73b8e755750ff460a1e1416c0e219e0
Valllerian <88560096+Valllerian@users.noreply.github.com>
2021-10-09 11:32:52 -0600

    Display works on load on page load (#30)
    * Testing git merge
    
    * Working on fetching simple movie lists (sorted by genre)
    
    * Every buttons now generates an array with movies connected to the holiday
    
    * Getting rid of genres - using search quary
    
    * updating vat names
    
    * Displaying dara on second page
    
    * Updating changes
    
    * Added trailer display
    
    * Working on display function for all the objects
    
    * Function for displaying is created
    
    * Display function works on pageload
    
    Co-authored-by: Laurel Thorburn <88687282+laurelthorburn@users.noreply.github.com>

--- Assets/javascript/movieScript.js
+++ Assets/javascript/movieScript.js
@@ -1,248 +1,34 @@
-// TMBD api keys
 
-const apiMovieKey = 'api_key=d58ec33864c2c1ca7cfddcf6e0b283c8';
-const baseUrl = 'https://api.themoviedb.org/3';
-const apiUrl = baseUrl + '/discover/movie?sort_by=popularity.desc&'+ apiMovieKey;
-const posterUrl = 'https://image.tmdb.org/t/p/w500';
-const searchURL = baseUrl + '/search/movie?'+ apiMovieKey;
+// const apiMovieKey = 'api_key=d58ec33864c2c1ca7cfddcf6e0b283c8';
+// const baseUrl = 'https://api.themoviedb.org/3';
+// const searchUrl = "https://api.themoviedb.org/3/search/movie?"
+// const language = "&language=en-US&";
+// const apiUrl = baseUrl + '/discover/movie?sort_by=popularity.desc&'+ apiMovieKey;
+// const posterUrl = 'https://image.tmdb.org/t/p/w500';
+// const searchURL = baseUrl + '/search/movie?'+ apiMovieKey;
 
-
-// Element that will show our search results
-const mainMovie = document.getElementById('mainMovie');
-
-// fetched genres from https://api.themoviedb.org/3/genre/movie/list?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8
-
-  const valentinesDay = $('#valentines');
-  const patrick = $('#patrick');
-  const independence = $('#independence');
-  const halloween = $('#halloween');
-  const thanksgiving = $('#thanksgiving');
-  const christmas = $('#christmas');
-  const newYear = $('#newYear');
-  const birthday = $('#birthday');
+//   var userChoice = localStorage.getItem('savedHoliday');
+//   var finalUrl =   searchUrl + apiMovieKey + language + "&query=" + userChoice + "&page=1&include_adult=false"
 
 
-  $('#valentines').on('click', function(){
-    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
-       // set up API fetch depending on holiday parameters
-       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Valentines&page=1&include_adult=false"
-      //  change ID automatically would be great
-    console.log("YAY you picked Valentine's");
-       fetch(finalUrl)
-       .then(function (response) {
-         // console.log(response)
-       return response.json();
-       })
-       .then(function (data) {
-         console.log(data) //i'm important
-         }
-   )
+
+//      $('#movie-trailer').on('click', function(){
+//       var trailer = "https://api.themoviedb.org/3/movie/716788/videos?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US"
+//       fetch(trailer).then(res => res.json()).then(videoData => {
+//           if(videoData.results.length > 0){
+//             var embed =  $('#testVideo');
+//             var embed = [];
+//               videoData.results.forEach((video, idx) => {
+//               let {name, key, site} = video
+//               if(site == 'YouTube'){
+//                 $('#videoTag').attr("src", "https://www.youtube.com/embed/" + video.key);
+//                 console.log(video.key)
+//                 console.log(video.name)
+//               }
+//             })
+//         }
+//      })});
+
+
   
-   });
 
-  $('#patrick').on('click', function(){
-    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
-       // set up API fetch depending on holiday parameters
-       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Patrick&page=1&include_adult=false"
-       //  change ID automatically would be great
- 
-        fetch(finalUrl)
-        .then(function (response) {
-          // console.log(response)
-        return response.json();
-        })
-        .then(function (data) {
-          console.log(data)
-          }
-    )
-    });
-
-  $('#halloween').on('click', function(){
-    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
-       // set up API fetch depending on holiday parameters
-       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Halloween&page=1&include_adult=false"
-       //  change ID automatically would be great
- 
-        fetch(finalUrl)
-        .then(function (response) {
-          // console.log(response)
-        return response.json();
-        })
-        .then(function (data) {
-          console.log(data)
-          }
-    )
-    });
-
-  $('#thanksgiving').on('click', function(){
-    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
-       // set up API fetch depending on holiday parameters
-       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Thanksgiving&page=1&include_adult=false"
-       //  change ID automatically would be great
- 
-        fetch(finalUrl)
-        .then(function (response) {
-          // console.log(response)
-        return response.json();
-        })
-        .then(function (data) {
-          console.log(data)
-          }
-    )
-    });
-
-  $('#christmas').on('click', function(){
-    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
-       // set up API fetch depending on holiday parameters
-       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Christmas&page=1&include_adult=false"
-       //  change ID automatically would be great
- 
-        fetch(finalUrl)
-        .then(function (response) {
-          // console.log(response)
-        return response.json();
-        })
-        .then(function (data) {
-          console.log(data)
-          }
-    )
-    });
-
-  $('#newYear').on('click', function(){
-    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
-       // set up API fetch depending on holiday parameters
-       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=NewYearEve&page=1&include_adult=false"
-       //  change ID automatically would be great
- 
-        fetch(finalUrl)
-        .then(function (response) {
-          // console.log(response)
-        return response.json();
-        })
-        .then(function (data) {
-          console.log(data)
-          }
-    )
-    });
-
-  $('#independence').on('click', function(){
-    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
-       // set up API fetch depending on holiday parameters
-       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=IndependenceDay&page=1&include_adult=false"
-       //  change ID automatically would be great
- 
-        fetch(finalUrl)
-        .then(function (response) {
-          // console.log(response)
-        return response.json();
-        })
-        .then(function (data) {
-          console.log(data)
-          }
-    )
-    });
-  $('#birthday').on('click', function(){
-    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
-       // set up API fetch depending on holiday parameters
-       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=birthday&page=1&include_adult=false"
-       //  change ID automatically would be great
- 
-        fetch(finalUrl)
-        .then(function (response) {
-          // console.log(response)
-        return response.json();
-        })
-        .then(function (data) {
-          console.log(data)
-          }
-    )
-    });
-
-    $('#test').on('click', function(){
-      // set up API fetch depending on holiday parameters
-      var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=2020&page=1&include_adult=false"
-      ;
-      //  change ID automatically would be great
-       fetch(finalUrl)
-         .then(function (response) {
-          // console.log(response)
-       return response.json();
-         })
-         .then(function (data) {
-           console.log(data)
-           var i = 1;
-           var imgLink = "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path;
-           
-           var description = data.results[i].overview;
-           var title = data.results[i].original_title
-           
-           var rating = data.results[i].vote_average
-           //  works for description fetch and display
-           $('#description').text(description);
-          //  works for title fetch and display
-           $('#title').text(title);
-          //  doesnt work
-           $('#posterIMGcard').attr("src", imgLink)
-           $('#rating').text("Rating: " + rating);
-         
-         })
-     });
-
-    
-
-     $('#movie-trailer').on('click', function(){
-      var trailer = "https://api.themoviedb.org/3/movie/716788/videos?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US"
-      fetch(trailer).then(res => res.json()).then(videoData => {
-          if(videoData.results.length > 0){
-            var embed =  $('#testVideo');
-            var embed = [];
-            
-            videoData.results.forEach((video, idx) => {
-              let {name, key, site} = video
-    
-              if(site == 'YouTube'){
-                  
-                $('#videoTag').attr("src", "https://www.youtube.com/embed/" + video.key);
-                console.log(video.key)
-                console.log(video.name)
-
-              }
-            })
-        }
-     })});
-
-
-//      $('#test').on('click', function(){
-//       // set up API fetch depending on holiday parameters
-//       var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=2020&page=1&include_adult=false"
-//       ;
-//       //  change ID automatically would be great
-//        fetch(finalUrl)
-//          .then(function (response) {
-//           // console.log(response)
-//        return response.json();
-//          })
-//          .then(function (data) {
-
-//           $("#movieCard").each(function (i) {
-//             var imgLink = "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path;
-//             var description = data.results[i].overview;
-//             var title = data.results[i].original_title
-//             var rating = data.results[i].vote_average
-
-//             $('#description').text(description);
-//           //  works for title fetch and display
-//            $('#title').text(title);
-//           //  doesnt work
-//            $('#posterIMGcard').attr("src", imgLink)
-//            $('#rating').text("Rating: " + rating);
-
-
-//             this.querySelector("#description").textContent = description;
-//             this.querySelector("#title").textContent = title;
-//             this.querySelector("posterIMGcard").setAttribute("src", imgLink);
-//             this.querySelector("#rating").textContent = "Rating: " + rating;
-//           });
-         
-//          })
-//      });
