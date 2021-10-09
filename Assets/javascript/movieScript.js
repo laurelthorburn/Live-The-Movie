b77648226c060c539e334e759444390531333da3
Valerii Bihun <valeriibihun.co@gmail.com>
2021-10-09 10:06:23 -0600

    Working on display function for all the objects

--- Assets/javascript/movieScript.js
+++ Assets/javascript/movieScript.js
@@ -1,10 +1,10 @@
 // TMBD api keys
 
-const apiKey = 'api_key=d58ec33864c2c1ca7cfddcf6e0b283c8';
+const apiMovieKey = 'api_key=d58ec33864c2c1ca7cfddcf6e0b283c8';
 const baseUrl = 'https://api.themoviedb.org/3';
-const apiUrl = baseUrl + '/discover/movie?sort_by=popularity.desc&'+ apiKey;
+const apiUrl = baseUrl + '/discover/movie?sort_by=popularity.desc&'+ apiMovieKey;
 const posterUrl = 'https://image.tmdb.org/t/p/w500';
-const searchURL = baseUrl + '/search/movie?'+ apiKey;
+const searchURL = baseUrl + '/search/movie?'+ apiMovieKey;
 
 
 // Element that will show our search results
@@ -16,35 +16,31 @@ const mainMovie = document.getElementById('mainMovie');
   const patrick = $('#patrick');
   const independence = $('#independence');
   const halloween = $('#halloween');
-  const thanksgiving = $('#thanksgiving');0
+  const thanksgiving = $('#thanksgiving');
   const christmas = $('#christmas');
   const newYear = $('#newYear');
   const birthday = $('#birthday');
 
-  // valentinesDay.addEventListener("click", openPage());
 
-  // function openPage() {
-    
-  // }
-
-  $('#Valentines').on('click', function(){
+  $('#valentines').on('click', function(){
     // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
        // set up API fetch depending on holiday parameters
        var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Valentines&page=1&include_adult=false"
       //  change ID automatically would be great
-
+    console.log("YAY you picked Valentine's");
        fetch(finalUrl)
        .then(function (response) {
          // console.log(response)
        return response.json();
        })
        .then(function (data) {
-         console.log(data)
+         console.log(data) //i'm important
          }
    )
+  
    });
 
-  $('#Patrick').on('click', function(){
+  $('#patrick').on('click', function(){
     // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
        // set up API fetch depending on holiday parameters
        var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Patrick&page=1&include_adult=false"
@@ -61,7 +57,7 @@ const mainMovie = document.getElementById('mainMovie');
     )
     });
 
-  $('#Halloween').on('click', function(){
+  $('#halloween').on('click', function(){
     // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
        // set up API fetch depending on holiday parameters
        var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Halloween&page=1&include_adult=false"
@@ -78,7 +74,7 @@ const mainMovie = document.getElementById('mainMovie');
     )
     });
 
-  $('#Thanksgiving').on('click', function(){
+  $('#thanksgiving').on('click', function(){
     // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
        // set up API fetch depending on holiday parameters
        var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Thanksgiving&page=1&include_adult=false"
@@ -91,11 +87,11 @@ const mainMovie = document.getElementById('mainMovie');
         })
         .then(function (data) {
           console.log(data)
-          })
-        
+          }
+    )
     });
 
-  $('#Christmas').on('click', function(){
+  $('#christmas').on('click', function(){
     // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
        // set up API fetch depending on holiday parameters
        var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=Christmas&page=1&include_adult=false"
@@ -112,7 +108,7 @@ const mainMovie = document.getElementById('mainMovie');
     )
     });
 
-  $('#NewYear').on('click', function(){
+  $('#newYear').on('click', function(){
     // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
        // set up API fetch depending on holiday parameters
        var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=NewYearEve&page=1&include_adult=false"
@@ -129,7 +125,7 @@ const mainMovie = document.getElementById('mainMovie');
     )
     });
 
-  $('#Independence').on('click', function(){
+  $('#independence').on('click', function(){
     // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
        // set up API fetch depending on holiday parameters
        var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=IndependenceDay&page=1&include_adult=false"
@@ -145,8 +141,8 @@ const mainMovie = document.getElementById('mainMovie');
           }
     )
     });
-  $('#Muertos').on('click', function(){
-       window.open("indexresults.html");
+  $('#birthday').on('click', function(){
+    // window.open("file:///C:/Users/derzo/HOMEWORK/Live-The-Movie/indexresults.html");
        // set up API fetch depending on holiday parameters
        var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=birthday&page=1&include_adult=false"
        //  change ID automatically would be great
@@ -198,8 +194,9 @@ const mainMovie = document.getElementById('mainMovie');
       var trailer = "https://api.themoviedb.org/3/movie/716788/videos?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US"
       fetch(trailer).then(res => res.json()).then(videoData => {
           if(videoData.results.length > 0){
+            var embed =  $('#testVideo');
             var embed = [];
-            var dots = [];
+            
             videoData.results.forEach((video, idx) => {
               let {name, key, site} = video
     
@@ -213,11 +210,7 @@ const mainMovie = document.getElementById('mainMovie');
               //     <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               
               // `)
-    
-              //   dots.push(`
-              //     <span class="dot">${idx + 1}</span>
-              //   `)
-
+  
               //   console.log(video.site)
 
               
@@ -228,32 +221,3 @@ const mainMovie = document.getElementById('mainMovie');
      })});
 
 
-
-  $('#test').on('click', function(){
-    // set up API fetch depending on holiday parameters
-    var finalUrl = "https://api.themoviedb.org/3/search/movie?api_key=d58ec33864c2c1ca7cfddcf6e0b283c8&language=en-US&query=birthday&page=1&include_adult=false"
-    //  change ID automatically would be great
-     fetch(finalUrl)
-       .then(function (response) {
-        // console.log(response)
-     return response.json();
-       })
-       .then(function (data) {
-         console.log(data)
-         var imgLink = "https://image.tmdb.org/t/p/w500" + data.results[4].backdrop_path;
-         var i = 1;
-         var description = data.results[i].overview;
-         var title = data.results[i].original_title
-         var posterImg = '<span class="card-title" style="width:100%; background: rgba(0, 0, 0, 0.5);">' + imgLink + '</span>'
-         var rating = data.results[i].vote_average
-         //  works for description fetch and display
-         $('#description').text(description);
-        //  works for title fetch and display
-         $('#title').text(title);
-        //  doesnt work
-        //  $('#posterIMG').text(posterIMG);
-         $('#rating').text("Rating: " + rating);
-        //  cant get it to create the element
-         console.log(posterImg);
-       })
-   });
